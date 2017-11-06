ed29cd314ff829a93c8ea2dd9e38e7d05b21d3e4
Aygyun <aygyun@gmail.com>
2017-11-06 09:16:02 -0800

    Update App to load the ArticleList

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import ArticleList from 'containers/ArticleList';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <ArticleList />
+      </div>
+    );
   }
 }
 
