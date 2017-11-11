bb975cbe3160c4a6b3387aff38253ed029b49b38
Aygyun <aygyun@gmail.com>
2017-11-11 13:43:35 -0800

    add Router to App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import Navbar from 'components/Navbar';
-import ArticleList from 'containers/ArticleList';
+import TechPage from 'containers/TechPage';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
+      <Router>
+        <div className="App">
+          <Navbar />
 
-        <ArticleList />
-      </div>
+          <Redirect exact path="/" to="/tech" />
+          <Route path="/tech" component={TechPage} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/containers/TechPage/TechPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Page from 'components/Page';
+import ArticleList from 'containers/ArticleList';
+
+const TechPage = () => (
+  <Page>
+    <ArticleList />
+  </Page>
+);
+
+export default TechPage;
