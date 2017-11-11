9c0edfa0df2940be7c9c642a4af5727e911f6d0d
Aygyun <aygyun@gmail.com>
2017-11-11 14:07:30 -0800

    update Articles module to use new networking modules

--- src/modules/Articles/Articles.js
+++ src/modules/Articles/Articles.js
@@ -1,21 +1,19 @@
-import data from './data.json';
-
 import axios from 'axios';
+
+import NewsAPI from 'modules/NewsAPI';
+
+import data from './data.json';
 
 const NEWS_API_KEY = 'f3ddd6f982ff4d96871f08cac80786d8';
 
 class Articles {
-  static fetch(params) {
-    let client = axios.create({
-      'Access-Control-Allow-Origin': '*',
-    });
+  static fetch({ source, sortBy }) {
+    const client = new NewsAPI();
+
     // await response of fetch call
     return client
-      .get('https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest', {
-        ...params,
-        headers: {
-          'x-api-key': NEWS_API_KEY,
-        },
+      .get('/', {
+        params: { source, sortBy },
       })
       .then(data => {
         const articles = data.data.articles;
