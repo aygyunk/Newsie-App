43f9388fe7e28bc81a6bd5dd31be97bd77e0385e
Aygyun <aygyun@gmail.com>
2017-11-11 14:08:29 -0800

    update TechPage to fetch techcrunch articles

--- src/containers/TechPage/TechPage.jsx
+++ src/containers/TechPage/TechPage.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Page from 'components/Page';
 import ArticleList from 'containers/ArticleList';
+import Articles from 'modules/Articles';
 
-const TechPage = () => (
-  <Page>
-    <ArticleList />
-  </Page>
-);
+class TechPage extends Component {
+  state = {
+    articles: [],
+  };
+
+  componentDidMount() {
+    Articles.fetch({ source: 'techcrunch', sortBy: 'latest' }).then(articles =>
+      this.setState(prevState => ({ ...prevState, articles })),
+    );
+  }
+
+  render() {
+    return (
+      <Page>
+        <ArticleList articles={this.state.articles} />
+      </Page>
+    );
+  }
+}
 
 export default TechPage;
