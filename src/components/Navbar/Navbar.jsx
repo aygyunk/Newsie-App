c4d22e8d4ff1b9e08476b2f93c3e8305259a2ea6
Aygyun <aygyun@gmail.com>
2017-11-11 13:43:55 -0800

    update Navbar to have links to various routes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import Logo from 'components/Logo';
 
@@ -9,6 +10,65 @@ class Navbar extends Component {
     return (
       <div className="Navbar">
         <Logo />
+
+        <div className="Navbar__links">
+          <NavLink
+            to="/world"
+            className="Navbar__links__link"
+            activeClassName="is-active"
+          >
+            <p>world</p>
+          </NavLink>
+          <NavLink
+            to="/sports"
+            className="Navbar__links__link"
+            activeClassName="is-active"
+          >
+            <p>sports</p>
+          </NavLink>
+          <NavLink
+            to="/tech"
+            className="Navbar__links__link"
+            activeClassName="is-active"
+          >
+            <p>tech</p>
+          </NavLink>
+          <NavLink
+            to="/arts"
+            className="Navbar__links__link"
+            activeClassName="is-active"
+          >
+            <p>arts</p>
+          </NavLink>
+          <NavLink
+            to="/fashion"
+            className="Navbar__links__link"
+            activeClassName="is-active"
+          >
+            <p>fashion</p>
+          </NavLink>
+          <NavLink
+            to="/business"
+            className="Navbar__links__link"
+            activeClassName="is-active"
+          >
+            <p>business</p>
+          </NavLink>
+          <NavLink
+            to="/travel"
+            className="Navbar__links__link"
+            activeClassName="is-active"
+          >
+            <p>travel</p>
+          </NavLink>
+          <NavLink
+            to="/environment"
+            className="Navbar__links__link"
+            activeClassName="is-active"
+          >
+            <p>environment</p>
+          </NavLink>
+        </div>
       </div>
     );
   }
